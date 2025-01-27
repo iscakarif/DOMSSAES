fabae45855eb943eff466ef0d55446701df1e71b
iscakarif <arif.iscak@proton.me>
2025-01-27 11:35:43 +0100

    Update SquareScaler.v

--- verilog/sources/SquareScaler.v
+++ verilog/sources/SquareScaler.v
@@ -3,38 +3,10 @@ module SquareScaler(
         output[1:0] out 
     );
     
-    //assign out[1:0] = in[1:0];
+        assign out[1] = in[1] ^ in[0];          // W-SquareScaler
+        assign out[0] = in[0];
     
-    //normal basis scalers
-    
-    //assign out[1] = in[1] ^ in[0];
-    //assign out[0] = in[1];
-    
-    //assign out[1] = in[0];
-    //assign out[0] = in[1] ^ in[0];
-    
-    assign out[1] = in[1] ^ in[0];
-    assign out[0] = in[0];
-    
-    //assign out[1] = in[1];
-    //assign out[0] = in[1] ^ in[0];
-    
-    
-    //---------------------------------
-    
-    //poly basis scalers
-    
-    //assign out[1] = in[1] ^ in[0];
-    //assign out[0] = in[1];
-    
-    //assign out[1] = in[0];
-    //assign out[0] = in[1] ^ in[0];
-    
-    //assign out[1] = in[0];
-    //assign out[0] = in[1];
-    
-    //assign out[1] = in[1] ^ in[0];
-    //assign out[0] = in[0];
-    
+        //assign out[1] = in[1];                //W2-SquareScaler
+        //assign out[0] = in[1] ^ in[0];
 
 endmodule
