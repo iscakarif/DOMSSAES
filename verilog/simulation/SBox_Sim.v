d42d5a08e1d67335e9105ca277b45207dea27615
iscakarif <arif.iscak@proton.me>
2025-01-27 11:37:25 +0100

    Update SBox_Sim.v

--- verilog/simulation/SBox_Sim.v
+++ verilog/simulation/SBox_Sim.v
@@ -1,11 +1,11 @@
 module SBox_Sim();
 
-    reg clk, reset;
-    reg[1:0] Z0, Z1, Z2, Z3, Z4, Z5;
+    reg clk;
+    reg[1:0] Z0, Z1, Z2, Az0, Az1, Az2, Bz0, Bz1, Bz2;
     reg[3:0] A, B;
     wire[3:0] A_out, B_out;
     
-    SBox sbox (.clk(clk), .Z0(Z0), .Z1(Z1), .Z2(Z2), .Z3(Z3), .Z4(Z4), .Z5(Z5), .A(A), .B(B), .A_out(A_out), .B_out(B_out));
+    SBox sbox (.clk(clk), .Z0(Z0), .Z1(Z1), .Z2(Z2), .Az0(Az0), .Az1(Az1), .Az2(Az2), .Bz0(Bz0), .Bz1(Bz1), .Bz2(Bz2), .A(A), .B(B), .A_out(A_out), .B_out(B_out));
     
     initial begin
         clk = 1'b0;
@@ -13,14 +13,12 @@ module SBox_Sim();
     end
     
     initial begin
-        reset = 1; 
-        Z0 = 2'b00; Z1 = 2'b00; Z2 = 2'b00; Z3 = 2'b00; Z4 = 2'b00; Z5 = 2'b00; A = 4'b0000; B = 4'b0000;
-        #20;
-        
-        reset = 0;
-        Z0 = 2'b10; Z1 = 2'b01; Z2 = 2'b11; Z3 = 2'b01; Z4 = 2'b01; Z5 = 2'b10; A = 4'b0000; B = 4'b0101;
+               
+        Az0 = 2'd0; Az1 = 2'd0; Az2 = 2'd0; Bz0 = 2'd0; Bz1 = 2'd0; Bz2 = 2'd0; Z0 = 2'd0; Z1 = 2'd0; Z2 = 2'd0; A = 4'd0; B = 4'd0;
         #100;
-        $stop;
+        
+        $finish;
+        
     end
 
 endmodule
