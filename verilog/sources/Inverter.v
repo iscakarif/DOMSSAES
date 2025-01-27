0d6863ab2d960b79266df6f7281f18616203f0f5
iscakarif <arif.iscak@proton.me>
2025-01-27 11:32:11 +0100

    Update Inverter.v

--- verilog/sources/Inverter.v
+++ verilog/sources/Inverter.v
@@ -3,14 +3,6 @@ module Inverter(
     output [1:0] out  
 );
     
-    //--polynomial basis--//
-    
-    //assign out[1] = in[1];
-    //assign out[0] = in[1] ^ in[0];
-
-    
-    //--normal basis--//
-
     assign out[1] = in[0];
     assign out[0] = in[1];
 
