34e3fcd21c1fd754b593effa2632d4976e0f4096
iscakarif <arif.iscak@proton.me>
2025-01-27 11:36:19 +0100

    Update DepMultiplier_Sim.v

--- verilog/simulation/DepMultiplier_Sim.v
+++ verilog/simulation/DepMultiplier_Sim.v
@@ -1,29 +1,22 @@
 module DepMultiplier_Sim();
 
-    reg clk, reset;
-    reg[1:0] Ax, Ay, Bx, By, Z0, Z1;
+    reg clk;
+    reg[1:0] Ax, Ay, Az, Bx, By, Bz, Z;
     wire[1:0] Aq, Bq;  
 
-    DepMultiplier dep (.clk(clk), .reset(reset), .Ax(Ax), .Ay(Ay), .Bx(Bx), .By(By), .Z0(Z0), .Z1(Z1), .Aq(Aq), .Bq(Bq));
+    DepMultiplier dep (.clk(clk), .Ax(Ax), .Ay(Ay), .Az(Az), .Bx(Bx), .By(By), .Bz(Bz), .Z(Z), .Aq(Aq), .Bq(Bq));
 
     initial begin
         clk = 1'b0;
-        forever #5 clk = ~clk;
+        forever #10 clk = ~clk;
     end
     
-    initial begin 
+    initial begin    
         
-        //Initialize inputs
-        reset = 1;
-        Ax = 2'b00; Ay = 2'b00; Bx = 2'b00; By = 2'b00; Z0 = 2'b00; Z1 = 2'b00;
-        #10;
+        Ax = 2'd2; Ay = 2'd1; Az = 2'd3; Bx = 2'd3; By = 2'd1; Bz = 2'd1; Z = 2'd2;
+        #100;
         
-        reset = 0;
-        
-        Ax = 2'b10; Ay = 2'b01; Bx = 2'b01; By = 2'b10; Z0 = 2'b01; Z1 = 2'b10;
-        #20;
-        
-        $stop;
+        $finish;
     end
 
 endmodule
