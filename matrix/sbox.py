53ab1cbf6bf034d7af673264c651ed5b1b91ec7f
iscakarif <arif.iscak@proton.me>
2025-02-02 23:58:50 +0100

    Update sbox.py now functional

--- matrix/sbox.py
+++ matrix/sbox.py
@@ -15,7 +15,15 @@ def multiply(x, y):
     else:
         return 0b11
 
+
+def indepABq(Ax, Ay, Bx, By, Z):
     
+    A_res = add(multiply(Ax,Ay), add(multiply(Ax, By), Z))
+    B_res = add(multiply(Bx,By), add(multiply(Bx, Ay), Z))
+    
+    return A_res, B_res
+
+
 def ABq(Ax, Ay, Bx, By, Z0, Z1):
     
     A_res = add(multiply(Ax, add(Ay, add(By, Z0))), add(multiply(Ax, Z0), Z1))
@@ -56,96 +64,182 @@ def inv(x):
     return x[::-1]
 
 
-def sqscl(x):
-    return [x[0] ^ x[1], x[1]] 
+def sqscl(x, w):
+    if w == 1:
+        return [x[0] ^ x[1], x[1]] 
+    else:
+        return [x[0], x[1]^ x[0]] 
 
 
 def gf2_matrix_multiply(A, B):
     return (np.dot(A, B) % 2)
 
 
-def SBox(A, B):
+def canright(x, m_in, m_out):
+    
+    transform = gf2_matrix_multiply(bin4ToArr(x), m_in)
+    
+    y = split(transform) # split(bin4ToArr(x)) 
+    y_0 = readBin(y[1])
+    y_1 = readBin(y[0])
+    
+    temp1 = y_0 ^ y_1
+    scl = sqscl(bin2ToArr(temp1),1)
+    y_01 = multiply(y_0, y_1)
+    
+    scl_y = readBin(scl) ^ y_01
+    inverse = inv(bin2ToArr(scl_y))
+    
+    res1 = multiply(readBin(inverse), y_0)
+    res0 = multiply(readBin(inverse), y_1)
+    
+    result = fuse(bin2ToArr(res1), bin2ToArr(res0))
+    
+    retransform = gf2_matrix_multiply(result, m_out)
+    
+    return readBin(retransform) # readBin(result)
+    
 
-    az0 = random.randint(0,3)
-    az1 = random.randint(0,3)
-    az2 = random.randint(0,3)
-    bz0 = random.randint(0,3)
-    bz1 = random.randint(0,3)
-    bz2 = random.randint(0,3)
+first_in = np.array([
+    [0,0,0,1],
+    [1,1,0,0],
+    [0,1,1,0],
+    [1,1,1,1]
+])
+
+first_out = np.array([
+    [1,0,0,0],
+    [1,1,1,1],
+    [0,1,0,0],
+    [1,1,1,0]
+])
+    
+first_inv = np.array([
+    [1,0,1,1],
+    [1,1,1,1],
+    [1,1,0,1],
+    [1,0,0,0]
+])
+
+second_in = np.array([
+    [0,1,0,0],
+    [0,0,1,1],
+    [1,0,0,1],
+    [1,1,1,1]
+])
+
+second_inv = np.array([
+    [1,1,0,1],
+    [1,0,0,0],
+    [1,0,1,1],
+    [1,1,1,1]
+])
+
+third_in = np.array([
+    [0,1,1,1],
+    [0,1,1,0],
+    [0,0,1,1],
+    [1,1,1,1]
+])
+
+third_inv = np.array([
+    [1,0,0,1],
+    [1,0,1,0],
+    [1,1,1,0],
+    [1,1,0,0]
+])
+
+fourth_in = np.array([
+    [1,1,0,1],
+    [1,0,0,1],
+    [1,1,0,0],
+    [1,1,1,1]
+])
+
+fourth_inv = np.array([
+    [1,1,1,0],
+    [1,1,0,0],
+    [1,0,0,1],
+    [1,0,1,0]
+])
+
+refirst_in = np.array([
+    [0,1,0,0],
+    [0,0,1,1],
+    [1,0,0,1],
+    [1,1,1,1]
+])
+
+refirst_out = np.array([
+    [0,0,1,0],
+    [1,1,1,1],
+    [0,0,0,1],
+    [1,0,1,1]
+])
+
+
+def inv_and_sbox(q, m_in, m_out):
     z0 = random.randint(0,3)
+    z10 = random.randint(0,3)
     z1 = random.randint(0,3)
+    z11 = random.randint(0,3)
     z2 = random.randint(0,3)
-    rng = [az0, az1, az2, bz0, bz1, bz2, z0, z1, z2]
-    #print("random shares:", rng)
+    z12 = random.randint(0,3)
+    b = random.randint(0,15)
+    a = q ^ b
+    print("q, A, B:",q,a,b)
     
-    M_in = np.array([
-        [0,0,0,1],
-        [1,1,0,0],
-        [0,1,1,0],
-        [1,1,1,1]
-    ])
-
-    M_out = np.array([
-        [1,0,0,0],
-        [1,1,1,1],
-        [0,1,0,0],
-        [1,1,1,0]
-    ])
+    a_tf = gf2_matrix_multiply(bin4ToArr(a), m_in)
+    b_tf = gf2_matrix_multiply(bin4ToArr(b), m_in)
     
-    M_inv = np.array([
-        [1,0,1,1],
-        [1,1,1,1],
-        [1,1,0,1],
-        [1,0,0,0]
-    ])
+    a1 = readBin(split(a_tf)[0])
+    a0 = readBin(split(a_tf)[1])
+    b1 = readBin(split(b_tf)[0])
+    b0 = readBin(split(b_tf)[1])
+    # print("A1:", a1, "A0", a0, "B1:", b1, "B0:", b0)
     
-    transformed_A = gf2_matrix_multiply(bin4ToArr(A), M_in)
-    transformed_B = gf2_matrix_multiply(bin4ToArr(B), M_in)
-    #print("A transformed:", transformed_A, "B transformed:", transformed_B)
+    a01 = a0 ^ a1
+    b01 = b0 ^ b1
+    # print("XOR:",a01,b01)
     
-    A_gamma_1 = readBin(transformed_A[0:2])
-    A_gamma_0 = readBin(transformed_A[2:4])
-    B_gamma_1 = readBin(transformed_B[0:2])
-    B_gamma_0 = readBin(transformed_B[2:4])
-    #print("A_gamma_1:", A_gamma_1, "A_gamma_0:", A_gamma_0, "B_gamma_1:", B_gamma_1, "B_gamma_0:", B_gamma_0)
+    a_sq = sqscl(bin2ToArr(a01),1)
+    b_sq = sqscl(bin2ToArr(b01),1)
+    # print("SqScale:", a_sq, b_sq)
     
-    A_gamma = A_gamma_0 ^ A_gamma_1
-    B_gamma = B_gamma_0 ^ B_gamma_1
+    dom = indepABq(a1, a0, b1, b0, z0)
+    # print("0 X 1:", dom[0], dom[1])
     
-    A_sqscl = sqscl(bin2ToArr(A_gamma))
-    B_sqscl = sqscl(bin2ToArr(B_gamma))
-    #print("A scaled:", A_sqscl, "B scaled:", B_sqscl)
+    a_xor = dom[0] ^ readBin(a_sq)
+    b_xor = dom[1] ^ readBin(b_sq)
+    # print("Pre Inv:",a_xor, b_xor)
     
-    dom = ABqx(A_gamma_1, B_gamma_0, B_gamma_1, A_gamma_0, az0, bz0, z0)
-    #print("Gamma1 mult Gamma2:", dom)
+    a_inv = readBin(inv(bin2ToArr(a_xor)))
+    b_inv = readBin(inv(bin2ToArr(b_xor)))
+    # print("Post Inv:",a_inv, b_inv)
     
-    domA_Asqscl = readBin(A_sqscl) ^ dom[0]
-    domB_Bsqscl = readBin(B_sqscl) ^ dom[1]
+    dom1 = indepABq(a1, a_inv, b1, b_inv, z1)
+    dom2 = indepABq(a_inv, a0, b_inv, b0, z2)
+    # print("O X 1:",dom1[0], dom1[1])
+    # print("O X 0:", dom2[0], dom2[1])
     
-    A_inv = readBin(inv(bin2ToArr(domA_Asqscl)))
-    B_inv = readBin(inv(bin2ToArr(domB_Bsqscl)))
-    #print("Inverse A:",A_inv,"Inverse B:", B_inv)
+    res_a = fuse(bin2ToArr(dom2[0]), bin2ToArr(dom1[0])) # fuse(bin2ToArr(dom1[0]), bin2ToArr(dom2[0]))
+    res_b = fuse(bin2ToArr(dom2[1]), bin2ToArr(dom1[1])) # fuse(bin2ToArr(dom1[1]), bin2ToArr(dom2[1]))
     
-    domA = ABqx(A_gamma_1, B_gamma_0, B_inv, A_inv, az1, bz1, z1)
-    domB = ABqx(B_gamma_1, A_gamma_0, A_inv, B_inv, az2, bz2, z2)
-    #print("Gamma1:", domA, "Gamma0:", domB)
+    a_outtf = gf2_matrix_multiply(res_a, m_out)
+    b_outtf = gf2_matrix_multiply(res_b, m_out)
     
-    preA = fuse(bin2ToArr(domA[0]), bin2ToArr(domB[1]))
-    preB = fuse(bin2ToArr(domA[1]), bin2ToArr(domB[0]))
-    #print("preA:", preA, "preB:", preB)
-   
-    retransform_A = gf2_matrix_multiply(preA, M_out)
-    retransform_B = gf2_matrix_multiply(preB, M_out)
-
-    return [readBin(retransform_A) ^ 6, readBin(retransform_B) ^ 6], (readBin(retransform_B) ^ 6) ^ (readBin(retransform_A) ^ 6) 
-
-
+    
+    
+    return readBin(a_outtf), readBin(b_outtf), readBin(a_outtf) ^ readBin(b_outtf), readBin(a_outtf) ^ 6, readBin(b_outtf), (readBin(a_outtf) ^ 6) ^ readBin(b_outtf)
+    
+    
+inverse = []
+stransform = []
 for i in range(16):
-    q = i
-    r = random.randint(0,15)
     
-    a = q ^ r
-    b = r
-
-    sbox = SBox(a, b)
-    print(sbox)
+    t = inv_and_sbox(i, first_in, first_inv)
+    s = inv_and_sbox(i, first_in, first_out)
+    inverse.append(t[2])
+    stransform.append(s[5])
+    
+print("Inverse:",inverse,"\nSBox:", stransform)    
