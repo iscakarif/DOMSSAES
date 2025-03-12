b571484166ebb2f308c0066a679f4469e74c7652
iscakarif <arif.iscak@proton.me>
2025-03-12 21:29:27 +0100

    Update AES444_Sim.v

--- verilog/DOMSSAES/simulation/AES444_Sim.v
+++ verilog/DOMSSAES/simulation/AES444_Sim.v
@@ -21,7 +21,7 @@ module AES444_Sim();
 	reg					start;
 	reg		[63:0]		key_in, k_mask;
 	reg		[63:0]		text_in, t_mask;
-	reg     [359:0]     r_bits;
+	reg     [359:0]     random_bits;
 	
 	wire	[63:0]		text_out;
 	
@@ -33,7 +33,7 @@ module AES444_Sim();
 	.text_in( text_in ),
 	.t_mask( t_mask ),
 	.k_mask( k_mask ),
-	.r_bits( r_bits ),
+	.random_bits( random_bits ),
 	.text_out( text_out )
 	);
 	
@@ -57,7 +57,7 @@ module AES444_Sim();
 		
 		t_mask = 64'h0;
 		k_mask = 64'h0;
-		r_bits = 359'h0;
+		random_bits = 359'h0;
 				
 		#100
 		
@@ -66,7 +66,7 @@ module AES444_Sim();
 		
 		t_mask = 64'hcf262e324a00edca;
 		k_mask = 64'h6865498b823f27f8;
-		r_bits = 359'h8225d4753f1bdf0270553d13bdcb32a22385e99d3fd9e1f05a5be33a8b6524a1bf54f9e30653e8ec2a3d2877ff;
+		random_bits = 359'h32d39e615212b905fdebd979817d8c88c314c05ca12b0df4def4db49378b64aa319f1cd732b2ce26d311b92de8;
 		
 		#100
 		
@@ -75,8 +75,41 @@ module AES444_Sim();
 		#15
 		
 		start = 1'b0;
+		random_bits = 359'h3ce2ca1f6f598d418992efa48038fb778634303f733e3b65c169c96bf2665d91bb9676379a1b2e7298addea3c5;
 		
-		#985
+		#80
+		
+		random_bits = 359'h0af19694c7918f80fca35fc6e0ca754023a815ca00c1206002d88f77a6baa122163cb02bb382fbeaa81a73e8ba;
+		
+		#80
+		
+		random_bits = 359'h253106ef890c5f0432801254b1da1ab51ab24788db728b6fb1756d4e69b88d134675d911dcffe9be5860517c10;
+		
+		#80
+		
+		random_bits = 359'h05006500bf527f2c21fd735772eb9c9a64a9fc581bd11f0e1a982bdaae3f5333bec424fec3bb787108cd07ee5b;
+		
+		#80
+		
+		random_bits = 359'h4882d9349a6db65a7b3a2a2b960319a8a88ce4e0ba78f0d2d9ec6db771bd6fab11c21fdff306c521c922f0e583;
+		
+		#80
+		
+		random_bits = 359'h2f7ab5242ba3fface28c867d9cbbfa5a437fe78983162d2537de36b2c9e64bea153a6d46af9043748531fe1208;
+		
+		#80
+		
+		random_bits = 359'h0040a51e2d37ccbfb3f8bef7f738963c26afc61fec568f7414f9794599053ca39e5dca77cdf1bd98bd3f196b2a;
+		
+		#80
+		
+		random_bits = 359'h3a1ee36c3fe7199b5ac05894433a2f384e062c155a7558a045dad3eecebdaf37c78979e30d92bbdfba4a1c22a8;
+		
+		#80
+		
+		random_bits = 359'h4882d9349a6db65a7b3a2a2b960319a8a88ce4e0ba78f0d2d9ec6db771bd6fab11c21fdff306c521c922f0e583;
+		
+		#265
 		
 		#300
 		
